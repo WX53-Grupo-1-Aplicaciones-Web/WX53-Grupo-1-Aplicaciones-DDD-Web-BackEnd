810f04d314490ca61691c649ddfa5d7926a59ea0
FranzJairLTV <134559608+FranzJairLTV@users.noreply.github.com>
2024-06-25 16:06:30 -0500

    Delete carpeta

--- Domain/Publishing/Models/Commands/ReceiptCommands/CreateReceiptCommand.cs
+++
@@ -1,9 +0,0 @@
-namespace Domain.Publishing.Models.Commands.ReceiptCommands;
-
-public class CreateReceiptCommand
-{
-    public string ProductId { get; set; }
-    public string Product { get; set; }
-    //public Dictionary<string, string> Parameters { get; set; }
-    public decimal Price { get; set; }
-}
